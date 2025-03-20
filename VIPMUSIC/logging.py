1d4aa160054dda73d9551fe1b84e8900a2c6a944
OGNOBITA12 <sumit07638@gmail.com>
2025-03-20 07:44:03 +0530

    Update logging.py

--- VIPMUSIC/logging.py
+++ VIPMUSIC/logging.py
@@ -1,7 +1,7 @@
 #
 # Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
 #
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
+# This file is part of < https://github.com/OGNOBITA12/HYPER_OP > project,
 # and is released under the MIT License.
 # Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
 #
